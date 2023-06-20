9990bc2d81e4c223ef87ea61354e10f1bc26db20
Luccas <luccassanches29@hotmail.com>
2023-06-20 14:26:06 -0300

    home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,19 @@
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
-      <div></div>
-      <div></div>
+      <div className="relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden" >
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 h-[288px] w-[526] bg-purple-700" />
+
+
+      </div>
+      <div className=" flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className=" text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="" className="underline hover:text-gray-50">criar agora</a>!
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
