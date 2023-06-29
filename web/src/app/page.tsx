c16abef26a729e4265eadc0e7ab8b3caa9397db7
Luccas <luccassanches29@hotmail.com>
2023-06-29 12:04:37 -0300

    rigth side home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,16 +1,44 @@
+import { User } from "lucide-react";
+import Image from 'next/image'
+
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
-      <div className="relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden" >
-        <div className="absolute right-0 top-1/2 -translate-y-1/2 h-[288px] w-[526] bg-purple-700" />
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        {/* Sigin */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+<Image src={nlwLogo} alt="NLW SPACE TIME LOGO" />
 
 
       </div>
-      <div className=" flex flex-col p-16">
+
+      {/* Direita */}
+      <div className=" flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
-          <p className=" text-center leading-relaxed w-[360px]">
+          <p className=" w-[360px] text-center leading-relaxed">
             Você ainda não registrou nenhuma lembrança, comece a{" "}
-            <a href="" className="underline hover:text-gray-50">criar agora</a>!
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
           </p>
         </div>
       </div>
